b401e8c60a5939a4c8344b199a22fd927d0e8b6f
Anas <anas.001falaki@gmail.com>
2024-05-29 14:21:06 +0100

    changes

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,10 @@
 export default function taskBlock(trueOrFalse) {
     let task = false;
-    const task2 = true;
+    let task2 = true;
   
     if (trueOrFalse) {
       let task = true;
-      const task2 = false;
+      let task2 = false;
     }
   
     return [task, task2];
