ba5ddb11cdac751db73a4761c39f197889fcc844
Anas <anas.001falaki@gmail.com>
2024-05-30 19:13:06 +0100

    arrow One line fuinction

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,1 @@
+let getSumOfHoods = (initialNumber, expansion1989, expansion2019) => expansion1989 = expansion1989 || 89, expansion2019 = expansion2019 || 19; return initialNumber + expansion1989 + expansion2019;
