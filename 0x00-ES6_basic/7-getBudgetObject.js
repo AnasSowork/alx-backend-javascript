3dd3f4857a164253355ba23e73287704966b9ee9
Anas <anas.001falaki@gmail.com>
2024-06-01 00:20:16 +0100

    Fix on task 7

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,3 +1,10 @@
 export default function getBudgetObject(income, gdp, capita) {
-  return { income, gdp, capita };
-}
+    const budget = {
+      income,
+      gdp,
+      capita,
+    };
+  
+    return budget;
+  }
+  
