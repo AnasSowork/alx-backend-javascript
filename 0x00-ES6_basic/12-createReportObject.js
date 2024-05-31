5c2ba8b389906987f8e5c5a45785856cdc135133
Anas <anas.001falaki@gmail.com>
2024-05-31 21:04:24 +0100

    ReportOb

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+    return {
+      allEmployees: {
+        ...employeesList,
+      },
+      getNumberOfDepartments() {
+        return Object.keys(this.allEmployees).length;
+      },
+    };
+  }
