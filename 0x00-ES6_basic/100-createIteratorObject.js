770d4ab5e71ff54053a75450ca9ce157dd8c2708
Anas <anas.001falaki@gmail.com>
2024-05-31 21:06:15 +0100

    iteratorOB

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,26 @@
+export default function createIteratorObject(report) {
+    const allEmployees = report.allEmployees;
+    let employees = [];
+  
+    for (const department in allEmployees) {
+      if (allEmployees.hasOwnProperty(department)) {
+        employees = employees.concat(allEmployees[department]);
+      }
+    }
+  
+    let currentIndex = 0;
+  
+    return {
+      [Symbol.iterator]() {
+        return {
+          next() {
+            if (currentIndex < employees.length) {
+              return { value: employees[currentIndex++], done: false };
+            } else {
+              return { done: true };
+            }
+          },
+        };
+      },
+    };
+  }
