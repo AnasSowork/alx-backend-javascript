4e167d54d4d6b44be5955052c930ab77c63f1c53
Anas <anas.001falaki@gmail.com>
2024-06-01 00:34:03 +0100

    Lint Fix on task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let idx = 0;
+  const newArray = [];
   for (const value of array) {
-    array[idx] = appendString + value;
-    idx++;
+    newArray.push(appendString + value);
   }
 
-  return array;
+  return newArray;
 }
