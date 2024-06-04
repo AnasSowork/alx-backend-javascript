57a5dade1cc3f81cba4e15c9ad4a4186116f3661
Anas <anas.001falaki@gmail.com>
2024-06-04 22:46:15 +0100

    fixes on lint

---
+++ 0x03-ES6_data_manipulation/eslint.config.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    ignores: ['node_modules/**'],
+  },
+  {
+    files: ['*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: 'LabeledStatement',
+          message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+        },
+        {
+          selector: 'WithStatement',
+          message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+        },
+      ],
+    },
+  },
+];
