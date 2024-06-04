1dd5e9aa4d173ecc6cfe4d7abc416a6318eff087
Anas <anas.001falaki@gmail.com>
2024-06-05 00:06:28 +0100

    Clean set fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,15 @@
 export default function cleanSet(set, startString) {
-    if (!startString || typeof startString !== 'string') {
-      return '';
-    }
-  
-    const result = [];
-  
-    for (const value of set) {
-      if (value.startsWith(startString)) {
-        result.push(value.slice(startString.length));
-      }
-    }
-  
-    return result.join('-');
+  if (!startString || typeof startString !== 'string') {
+    return '';
   }
-  
+
+  const result = [];
+
+  for (const value of set) {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  }
+
+  return result.join('-');
+}
