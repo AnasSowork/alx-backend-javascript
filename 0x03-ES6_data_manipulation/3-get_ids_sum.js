ecd3537d463db9a3e74824aa57aba6c9c6f6bf96
Anas <anas.001falaki@gmail.com>
2024-06-04 23:55:05 +0100

    Get ids and update

---
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,4 @@
+export default function getStudentIdsSum(students) {
+    return students.reduce((sum, student) => sum + student.id, 0);
+  }
+  
---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+    return students
+      .filter(student => student.location === city)
+      .map(student => {
+        const gradeObj = newGrades.find(grade => grade.studentId === student.id);
+        return {
+          ...student,
+          grade: gradeObj ? gradeObj.grade : 'N/A',
+        };
+      });
+  }
+  
